6ee9cae18cb4ddb95f9ce37692f3b32f7e40cb67
冯晨#A04763 <chen.feng@casstime.com>
2022-09-06 17:27:31 +0800

    响应式布局

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 // import Index from "./src_01_StyleSheet"; 
 // import Index from "./src_02_FlexBox/FlexDriection";
 // import Index from "./src_02_FlexBox/JustifyContent";
-import Index from "./src_02_FlexBox/AlignItems";
+// import Index from "./src_02_FlexBox/AlignItems";
+import Index from './src_03_Dimension/index';
 import { StatusBar } from 'expo-status-bar';
 import { render } from 'react-dom';
 
